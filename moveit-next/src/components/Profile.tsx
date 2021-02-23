978daf7164fd0d09960e871c76e7256af5feb00d
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-02-23 14:08:33 -0300

    Alterado o projeto para utilizar nextjs

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,12 @@
+export function Profile()
+{
+    return (
+        <div>
+            <img src="https://github.com/Alessandro-Miranda.png" alt="Foto de perfil" />
+            <div>
+                <strong>Alessandro Miranda</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
