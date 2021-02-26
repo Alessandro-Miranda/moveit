6749a2a3513e0398d3bcb715875b673b6d582066
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-02-26 11:27:49 -0300

    Modificado o tempo dos ciclos

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -24,7 +24,7 @@ export function CountdownProvider({ children }: CountdownProviderProps)
 {
     const { startNewChallenge } = useContext(ChallengesContext);
 
-    const [ time, setTime ] = useState(.1 * 60);
+    const [ time, setTime ] = useState(25 * 60);
     const [ isActive, setIsActive ] = useState(false);
     const [ hasFinished, setHasFinished ] = useState(false);
 
@@ -41,7 +41,7 @@ export function CountdownProvider({ children }: CountdownProviderProps)
         clearTimeout(countdownTimeout);
         setHasFinished(false);
         setIsActive(false);
-        setTime(.1 * 60);
+        setTime(25 * 60);
     }
 
     useEffect(() => {
